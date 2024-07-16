65adb81761d2c88287bbe6dedd0ee579405135e8
Daniel Kadyrov <daniel.kadyrov@gmail.com>
2024-07-16 08:13:54 -0400

    added files

---
+++ helloworld.py
@@ -0,0 +1,55 @@
+#%%
+print("hello world!")
+
+#%%
+# Variables 
+x = 10 
+y = 20
+
+print(x+y)
+
+z = x + y
+
+# %%
+# Integers
+
+x = 1 
+
+# Floats
+y = 2.8 
+
+z = x+y
+# %%
+# Lists 
+
+my_list = [1, 2, 3, 4, 5]
+
+
+#%%
+# Dictionaries 
+my_dictionary = {
+    "First": "Daniel",
+    "Last": "Kadyrov",
+    "Age": 31, 
+    "Interests": ["Surfing", "Yoga", "Python"],
+    "Parents": {
+        "Mother": "Anya",
+        "Father": "Marat"
+    }
+}
+# %%
+my_string = "Daniel Kadyrov"
+# %%
+problem = "This is another problem"
+# %%
+x = 12 
+y = 5 
+
+print("X is equal to: " + str(x))
+print(f"Y is equal to: {y}")
+print(f"X/Y is equal to: {x // y} remainder {x % y}")
+
+
+# %%
+x = "This my variable"
+# %%
---
+++ kadyrov_daniel_assignment1.py
@@ -0,0 +1,24 @@
+#%%
+first_name = "Daniel"
+last_name = "Kadyrov"
+
+height_feet = 5
+height_inches = 11
+
+total_height_in_inches = height_feet * 12 + height_inches
+
+list_of_interests = ["Surfing", "Biking", "Surfing"]
+
+profile = {
+    "First": first_name, 
+    "Last": last_name, 
+    "Height (feet)": total_height_in_inches // 12,
+    "Height (inches)": total_height_in_inches % 12,
+    "Interests": list_of_interests, 
+    "Number of Interests": len(list_of_interests)
+}
+
+#%%
+paragraph = f"{profile["First"]} {profile["Last"]} is {profile["Height (feet)"]}\' {profile["Height (inches)"]}\" tall. They have {profile["Number of Interests"]} interests."
+print(paragraph)
+# %%
